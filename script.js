cbcf9293505ad97e06c0f556d4be2007a5dd9bc1
Zaath <zebigmatt@hotmail.com>
2022-01-30 15:52:21 +0100

    Finish img animation

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let persoImg = document.getElementById('profile-pic');
+let persoCaption = document.getElementById('profile-pic-caption');
 
 function replaceImg () {
     persoImg.src = "images/crazy-me.jpg";
+    persoCaption.innerHTML = "Ugly, isn't?"
 };
 
 persoImg.addEventListener("click", replaceImg);
